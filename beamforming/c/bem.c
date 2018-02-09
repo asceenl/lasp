10b7ba60f8ae6b690cadc0ac3f3841613820742f
J.A. de Jong <j.a.dejong@ascee.nl>
2018-02-09 11:57:11 +0100

    Started on implementation of BEM

---
+++ beamforming/c/bem.c
@@ -0,0 +1,20 @@
+// bem.c
+//
+// Author: J.A. de Jong -ASCEE
+// 
+// Description:
+//
+//////////////////////////////////////////////////////////////////////
+#include "types.h"
+
+typedef struct {
+    umat elem;
+    vd nodes;
+    ElemType elemtype;
+} BemMesh;
+
+int Ms_osc_free(BemMesh* mesh,cmat* Ms) {
+
+
+}
+//////////////////////////////////////////////////////////////////////
